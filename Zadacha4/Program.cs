b0208698476dad066301093d2dbff475486a8b89
Svyatoslav Potrashkov <stri_69@mail.ru>
2022-11-12 21:28:52 +0300

    Zadacha4 task solved

--- Zadacha4/Program.cs
+++ Zadacha4/Program.cs
@@ -3,3 +3,27 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < num2) {
+    if (num2 < num3) {    
+    Console.WriteLine($"Число {num3} - большее");
+    }
+    else {
+    Console.WriteLine($"Число {num2} - большее");
+    }
+}
+
+else if (num1 < num3) {
+    Console.WriteLine($"Число {num3} - большее");
+}
+
+else {
+    Console.WriteLine($"Число {num1} - большее");
+}
