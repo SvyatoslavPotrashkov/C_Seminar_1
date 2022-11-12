59c222d56923ae40af560cf0b0693e8aea169ead
Svyatoslav Potrashkov <stri_69@mail.ru>
2022-11-13 00:29:33 +0300

    Zadacha8 task solved

--- Zadacha8/Program.cs
+++ Zadacha8/Program.cs
@@ -2,3 +2,13 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++) 
+{
+    if (i % 2 == 0) {
+    Console.WriteLine($"{i}");
+}
+}
